88f2998f1b4b78b75128976170e91478791f9c1c
NathanBatistaBalanche <nathan.batista_balanche@edu.univ-fcomte.fr>
2025-11-19 16:20:32 +0100

    referme les détails suite à un clic sur un `summary`

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
-// *** ACCORDEON ***
+// Récupération de l'accordéon
+const accordeon = document.querySelector(".accordeon");
+
+// Fermer tous les details au départ
+accordeon.querySelectorAll("details").forEach((det) => (det.open = false));
+
+// Ajouter un événement à chaque details
+accordeon.querySelectorAll("details").forEach((det) => {
+    det.addEventListener("toggle", () => {
+        if (det.open) {
+            // Fermer tous les autres details
+            accordeon.querySelectorAll("details").forEach((other) => {
+                if (other !== det) {
+                    other.open = false;
+                }
+            });
+        }
+    });
+});
